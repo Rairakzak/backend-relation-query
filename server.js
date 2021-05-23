9996ffa8a6dfcd7d63cffcbc80321c8ec0b85d65
Rakan Douli <rairakzak@gmail.com>
2021-05-23 03:10:47 +0200

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { user, todolist, todoitem, tag, itemtag } = require("./models");
-// const cors = require("cors");
-const PORT = 4001;
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.get("/users", async (req, res) => {
   try {
